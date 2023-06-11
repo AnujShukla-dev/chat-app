cebabb997ef3401736962113940b1273923b39fd
anuj-shukla <anujshukla575@gmail.com>
2023-06-11 17:58:21 +0530

    acknowledgement implementation'

--- src/index.js
+++ src/index.js
@@ -1,36 +1,47 @@
 const path = require("path");
-const http = require('http');
-const express =  require('express');
-const socketio = require('socket.io')
-const app = express()
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const app = express();
+const Filter = require("bad-words");
 const server = http.createServer(app);
-const io = socketio(server)
+const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
-// let count =0;
-io.on('connection',(socket)=>{
-    console.log('New Websocket connection')
+io.on("connection", (socket) => {
+  console.log("New Websocket connection");
+  socket.emit("welcomeMessage", "Welcome!");
+  socket.broadcast.emit("welcomeMessage", "A new user has joined");
+  socket.on("messageSend", (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback("Bad words not allowed");
+    }
+    io.emit("welcomeMessage", message);
+    callback("delivered!");
+  });
+  socket.on("sendLocation", ({ latitude, longitude },callback) => {
     
-     socket.emit('welcomeMessage','Welcome!')
-     socket.broadcast.emit('welcomeMessage','A new user has joined')
-     socket.on('messageSend',(message)=>{
-        io.emit('welcomeMessage',message)
-     })
-    // socket.on('increment',()=>{
-    //     count++,
-    //     io.emit('countUpdated',count)
-    // })
+    io.emit(
+      "welcomeMessage",
+      `https://google.com/maps?q=${latitude},${longitude}`
+    );
+    callback('Location Shared')
+  });
+  // socket.on('increment',()=>{
+  //     count++,
+  //     io.emit('countUpdated',count)
+  // })
 
-    socket.on('disconnect',()=>{
-        io.emit('welcomeMessage',"User has left")
-    })
-})
-
+  socket.on("disconnect", () => {
+    io.emit("welcomeMessage", "User has left");
+  });
+});
 
 // app.get("", (req, res) => {
 //     res.render("index");
 //   });
-server.listen(3000,()=>{
-     console.log(' Server running on localhost 3000')
-})
+server.listen(3000, () => {
+  console.log(" Server running on localhost 3000");
+});
